8a528283549f818133ebe6010d26332b7c8d50fc
Kyle Sunderland <sunderlandkyl@gmail.com>
2017-12-21 12:00:51 -0500

    Improve PlusRemote server control
    * Rearrange server buttons
    * Allow user to load text file into vtkMRMLTextNode (Still need to set up storage node to automatically update from file)
    * Allow users to set verbosity for server
    * Add Server log field
    * Combine start and stop buttons into a single button
    * Update server state based on callbacks (currently cant tell if a process was successful or not with callback)

--- OpenIGTLinkRemote/Logic/vtkSlicerOpenIGTLinkRemoteLogic.h
+++ OpenIGTLinkRemote/Logic/vtkSlicerOpenIGTLinkRemoteLogic.h
@@ -62,8 +62,8 @@ public:
   ///     cmd.AddObserver(slicer.modulelogic.vtkSlicerOpenIGTLinkCommand.CommandCompletedEvent, notificationMethod)
   bool SendCommand(vtkSlicerOpenIGTLinkCommand* command, const char* connectorNodeId);
 
-  /// TODO:REMOVE
-  bool SendCommand(std::string connectorNodeId, std::string device_id, std::string command, std::string content);
+  /// TODO: Python can't wrap igtlio::DevicePointer, have to use object pointer instead
+  vtkObject* SendCommand(std::string connectorNodeId, std::string device_id, std::string command, std::string content, vtkCallbackCommand* responseCallback = NULL);
 
   /// Cancel a command: removes from the OpenIGTLink connector's query queue, removes the
   /// association with the query node (so that it is reusable for sending another command),
